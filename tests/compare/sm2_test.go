e50be9cf3326b89ff4df5728c90fedcf9d14f524
kane <kane@localhost.localdomain>
2019-04-09 04:08:41 -0400

    add sm2

---
+++ tests/compare/sm2_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/pem"
+	"fmt"
+	"testing"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/guanzhi/GmSSL/go/gmssl"
+	"github.com/stretchr/testify/assert"
+	"github.com/tjfoc/gmsm/sm2"
+)
+
+func TestSM2Result(t *testing.T) {
+	//secp256k1
+	priv0, err := btcec.NewPrivateKey(btcec.S256())
+	assert.Nil(t, err)
+	bytez := priv0.Serialize()
+	priv1, _ := btcec.PrivKeyFromBytes(btcec.S256(), bytez)
+	assert.Equal(t, priv0, priv1)
+
+	//tjfoc
+	priv_tj0, err := sm2.GenerateKey()
+	assert.Nil(t, err)
+	bytez, err = sm2.MarshalSm2UnecryptedPrivateKey(priv_tj0)
+	assert.Nil(t, err)
+	priv_tj1, err := sm2.ParsePKCS8UnecryptedPrivateKey(bytez)
+	assert.Nil(t, err)
+	assert.Equal(t, priv_tj0, priv_tj1)
+
+	block := &pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: bytez,
+	}
+	pemBytes := pem.EncodeToMemory(block)
+	priv_tj2, err := sm2.ReadPrivateKeyFromMem(pemBytes, nil)
+	fmt.Println(len(pemBytes))
+	assert.Equal(t, priv_tj0, priv_tj2)
+
+	//gmssl
+	sm2keygenargs := map[string]string{
+		"ec_paramgen_curve": "sm2p256v1",
+		"ec_param_enc":      "named_curve",
+	}
+	sm2sk, err := gmssl.GeneratePrivateKey("EC", sm2keygenargs, nil)
+	assert.Nil(t, err)
+	sm2skpem, err := sm2sk.GetPEM("SMS4", "pass")
+	assert.Nil(t, err)
+	fmt.Println(sm2skpem, "=====", len(sm2skpem), "========")
+	priv_ssl0, err := gmssl.NewPrivateKeyFromPEM(sm2skpem, "pass")
+	privText, err := priv_ssl0.GetText()
+	assert.Nil(t, err)
+	assert.Equal(t, sm2sk, priv_ssl0)
+	fmt.Println(privText)
+}
