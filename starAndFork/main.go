cddaf7fb3c01b19af7cb8acab508c674284ee764
needkane <e.zl@qq.com>
2020-03-03 01:17:17 +0800

    update star and fork

--- starAndFork/main.go
+++ starAndFork/main.go
@@ -10,16 +10,30 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/dappledger/AnnChain/eth/crypto"
 	"github.com/urfave/cli"
 )
 
+type QueryJsonV3 struct {
+}
+type StarInfoV3 struct {
+	StarredAt string `json:"starred_at"`
+	User      User   `json:"user"`
+}
+type User struct {
+	Login string `json:"login"`
+}
 type QueryJson struct {
 	Query string `json:"query"`
 }
 type StarForkResult struct {
 	Data Data `json:"data"`
+	//failed response
+	Message string `json:"message"`
+}
+type FailedResponse struct {
 }
 type Data struct {
 	Repository Repository `json:"repository"`
@@ -43,7 +57,14 @@ type Node struct {
 	Url string `json:"url"`
 }
 
-const queryContent = `query { repository(owner:"dappledger", name:"AnnChain") { stargazers (last : 100){ totalCount edges {  node { url } } }  forks(last : 100) { totalCount edges { node { url  } } } } }`
+var (
+	bearToken    = "876aaf3d250742330fd3246d9e44b7f7002e0dcb"
+	pageCount    = 30
+	maxLastCount = 100
+	queryContent = fmt.Sprintf(`query { repository(owner:"dappledger", name:"AnnChain") { stargazers (last : %d){ totalCount edges {  node { url } } }  forks(last : %d) { totalCount edges { node { url  } } } } }`, maxLastCount, maxLastCount)
+	apiV3StarUrl = "https://api.github.com/repos/dappledger/annchain/stargazers"
+	apiV3ForkUrl = "https://api.github.com/repos/dappledger/annchain/fork"
+)
 
 func main() {
 	app := cli.NewApp()
@@ -70,6 +91,57 @@ type Result struct {
 	Address  string `json:"address"`
 }
 
+func queryByAPIV3(url, username string, surplus int) error {
+	pages := surplus / pageCount
+	if surplus%pageCount != 0 {
+		pages += 1
+	}
+	var wg sync.WaitGroup
+	wg.Add(pages)
+	getInfo := make(chan bool, 1)
+	getInfo <- false
+	fGetStarInfo := func(pageNo int) {
+		req, err := http.NewRequest("GET", fmt.Sprintf("%s?page=%d&access_token=%s", url, pageNo, bearToken), nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.Header.Set("Accept", "application/vnd.github.v3.star+json")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatalf("error is (%v),please check your network to make sure you can connect to github api", err)
+		}
+		defer resp.Body.Close()
+		bytez, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var siV3s = []StarInfoV3{}
+		err = json.Unmarshal(bytez, &siV3s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, v := range siV3s {
+			if v.User.Login == username {
+				<-getInfo
+				getInfo <- true
+				break
+			}
+		}
+		wg.Done()
+	}
+	for i := 1; i <= pages; i++ {
+		j := i
+		go func() {
+			fGetStarInfo(j)
+		}()
+	}
+	wg.Wait()
+	if <-getInfo {
+		return nil
+	}
+	return fmt.Errorf("%s, please star and fork github.com/dappledger/AnnChain", username)
+}
 func queryByGithubAPI(ctx *cli.Context) error {
 	if ctx.String("username") == "" {
 		return errors.New("Please specify a username by '-u [Your github username]'")
@@ -83,7 +155,7 @@ func queryByGithubAPI(ctx *cli.Context) error {
 	if err != nil {
 		log.Fatal(err)
 	}
-	req.Header.Set("Authorization", "bearer [Your github account token]")
+	req.Header.Set("Authorization", fmt.Sprintf("bearer %s", bearToken))
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -94,28 +166,48 @@ func queryByGithubAPI(ctx *cli.Context) error {
 	if err != nil {
 		log.Fatal(err)
 	}
-	sfr := StarForkResult{}
+	var sfr = new(StarForkResult)
 	err = json.Unmarshal(bytez, &sfr)
 	if err != nil {
 		log.Fatal(err)
 	}
+	if sfr.Message != "" {
+		return fmt.Errorf("query failed: %s", sfr.Message)
+	}
+
 	username := ctx.String("username")
-	err = fmt.Errorf("%s, please star github.com/dappledger/AnnChain", username)
+	err = fmt.Errorf("%s, please star and fork github.com/dappledger/AnnChain", username)
 	for _, v := range sfr.Data.Repository.Stargazers.Edges {
 		if v.Node.Url[19:] == username {
 			err = nil
 			break
 		}
 	}
+	// query history forks too slow
+	ignoreQueryForks := false
 	if err != nil {
-		return err
+		if sfr.Data.Repository.Stargazers.TotalCount > maxLastCount {
+			err = queryByAPIV3(apiV3StarUrl, username, sfr.Data.Repository.Stargazers.TotalCount-maxLastCount)
+			if err != nil {
+				return err
+			} else {
+				ignoreQueryForks = true
+			}
+		} else {
+			return err
+		}
 	}
-	err = fmt.Errorf("%s, please fork github.com/dappledger/AnnChain", username)
-	for _, v := range sfr.Data.Repository.Forks.Edges {
-		l := len(v.Node.Url)
-		if v.Node.Url[19:l-9] == username {
-			err = nil
-			break
+	if !ignoreQueryForks {
+		err = fmt.Errorf("%s, please fork github.com/dappledger/AnnChain", username)
+		for _, v := range sfr.Data.Repository.Forks.Edges {
+			l := len(v.Node.Url)
+			if v.Node.Url[19:l-9] == username {
+				err = nil
+				break
+			}
+		}
+		if err != nil {
+			return err
 		}
 	}
 	result, err := createAccount()
